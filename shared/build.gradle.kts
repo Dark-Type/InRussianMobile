b47a589651d83ba8f5c9d05118c2f84c67687669
Dark-Type <SKAZHUTIN.N@MAIL.RU>
2025-08-16 16:56:47 +0700

    🚧 Fixed the ios framework workflow

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,34 +1,48 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinSerialization)
+
 }
 
 kotlin {
+    applyDefaultHierarchyTemplate() 
+
     // --- Android target ---
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
+
     }
 
     // --- iOS targets ---
-    val iosX64Target = iosX64()
-    val iosArm64Target = iosArm64()
-    val iosSimArm64Target = iosSimulatorArm64()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    )
+        .forEach { iosTarget ->
+            iosTarget.binaries.framework {
+                baseName = "Shared"
+
+                export(libs.decompose)
+                export(libs.essenty.lifecycle)
+
+            }
+        }
 
     sourceSets {
         val commonMain by getting {
             dependencies {
+
+                api(libs.decompose)
+                api(libs.essenty.lifecycle)
+
                 implementation(libs.kotlinx.coroutines.core)
-                implementation(libs.decompose)
                 implementation(libs.mvikotlin)
                 implementation(libs.mvikotlin.main)
                 implementation(libs.mvikotlin.coroutines)
@@ -36,79 +50,7 @@ kotlin {
                 implementation(libs.kotlinx.serialization.json)
             }
         }
-        val commonTest by getting {
-            dependencies {
-                implementation(libs.kotlin.test)
-                implementation(libs.mvikotlin.main)
-            }
-        }
 
-        val commonComposeMain by creating {
-            dependsOn(commonMain)
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
-                implementation(compose.components.resources)
-                implementation(compose.components.uiToolingPreview)
-
-                implementation(libs.decompose.compose)
-                implementation(libs.koin.compose)
-            }
-        }
-
-        val androidMain by getting {
-            dependsOn(commonComposeMain)
-        }
-
-
-        val nativeMain by creating {
-            dependsOn(commonMain)
-        }
-        val nativeTest by creating {
-            dependsOn(commonTest)
-        }
-
-        val iosMain by creating {
-            dependsOn(nativeMain)
-
-            dependencies {
-                implementation(compose.runtime)
-            }
-        }
-        val iosTest by creating {
-            dependsOn(nativeTest)
-        }
-
-        val iosX64Main by getting {
-            dependsOn(iosMain)
-        }
-        val iosArm64Main by getting {
-            dependsOn(iosMain)
-        }
-        val iosSimulatorArm64Main by getting {
-            dependsOn(iosMain)
-        }
-
-        val iosX64Test by getting {
-            dependsOn(iosTest)
-        }
-        val iosArm64Test by getting {
-            dependsOn(iosTest)
-        }
-        val iosSimulatorArm64Test by getting {
-            dependsOn(iosTest)
-        }
-    }
-
-    val xcf = XCFramework()
-    listOf(iosX64Target, iosArm64Target, iosSimArm64Target).forEach { target ->
-        target.binaries.framework {
-            baseName = "Shared"
-            isStatic = true
-            xcf.add(this)
-        }
     }
 }
 
@@ -120,21 +62,14 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
 
-    sourceSets {
-        named("main") {
-            kotlin.srcDir("src/androidMain/kotlin")
-            res.srcDir("src/androidMain/res")
-            manifest.srcFile("src/androidMain/AndroidManifest.xml")
-        }
-
-        named("androidTest") {
-            kotlin.srcDir("src/androidTest/kotlin")
-        }
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+
+    sourceSets.getByName("main") {
+        manifest.srcFile("src/androidMain/AndroidManifest.xml")
     }
 }
 
